6f057fd7b4df11a9a080f26157960f4370b37d71
Tiago Resende <tiagor87@gmail.com>
2015-08-02 15:53:23 -0300

    Implementa diretiva de input para data

---
+++ src/n4dateinput.js
@@ -0,0 +1,69 @@
+"use strict";
+
+;
+(function(ng) {
+  ng
+    .module('n4DateInput', [])
+    .directive('n4DateInput', [
+      '$filter',
+      function($filter) {
+        return {
+          require: 'ngModel',
+          restrict: 'EAC',
+          replace: true,
+          template: '<input type="text">',
+          link: function(scope, element, attrs, controller) {
+            var formatValue = function(value) {
+                if (!value) {
+                  return '';
+                }
+
+                var numbers, regexp, date,
+                  returnValue = function(dateValue) {
+                    return isNaN(dateValue) ? '' : $filter('date')(dateValue, 'dd/MM/yyyy');
+                  };
+
+                if (value instanceof Date) {
+                  return returnValue(value);
+                }
+
+                numbers = value.replace(/\D/gi, '');
+
+                regexp = new RegExp('^(\\d{2})(\\d{2})(\\d{2})$', 'gi');
+                if (regexp.test(numbers)) {
+                  date = new Date(numbers.replace(regexp, '$2/$1/' + (attrs.century || '20') + '$3'));
+                  return returnValue(date);
+                }
+
+                regexp = new RegExp('^(\\d{2})(\\d{2})(\\d{4})$', 'gi');
+                if (regexp.test(numbers)) {
+                  date = new Date(numbers.replace(regexp, '$2/$1/$3'));
+                  return returnValue(date);
+                }
+
+                regexp = new RegExp('^\\w{3} \\w{3} \\d{1,2} \\d{4} .*$', 'gi');
+                if (regexp.test(value)) {
+                  return returnValue(new Date(value));
+                }
+
+                return value;
+              },
+              parseValue = function(value) {
+                var formattedValue = formatValue(value),
+                  regexp = new RegExp('^(\\d{2})/(\\d{2})/(\\d{4})$', 'gi');
+
+                return regexp.test(formattedValue) ? new Date(formattedValue.replace(regexp, '$2/$1/$3')) : null;
+              };
+
+            if (!element.attr('placeholder')) {
+              element.attr('placeholder', '00/00/0000');
+            }
+            element.attr('maxlength', 10);
+
+            controller.$formatters.push(formatValue);
+            controller.$parsers.push(parseValue);
+          }
+        };
+      }
+    ]);
+}(angular))
---
+++ tests/n4dateinput_test.js
@@ -0,0 +1,141 @@
+'use strict';
+
+describe('n4DateInput', function() {
+  var $scope, $compile;
+
+  beforeEach(module('n4DateInput'));
+
+  beforeEach(inject(function(_$rootScope_, _$compile_) {
+    $scope = _$rootScope_.$new();
+    $compile = _$compile_;
+  }));
+
+  describe('Creation', function() {
+    it('Should be able to create directive by element', function() {
+      var element = angular.element('<n4-date-input data-ng-model="value"></n4-date-input>');
+      $compile(element)($scope);
+      $scope.$apply();
+
+      expect(angular.isDefined(element)).toBeTruthy();
+      expect(element[0].tagName).toBe('INPUT');
+      expect(element[0].placeholder).toBe('00/00/0000');
+      expect(element[0].maxLength).toBe(10);
+    });
+
+    it('Should be able to create directive by class', function() {
+      var element = angular.element('<input class="n4-date-input" data-ng-model="value">');
+      $compile(element)($scope);
+      $scope.$apply();
+
+      expect(angular.isDefined(element)).toBeTruthy();
+      expect(element[0].tagName).toBe('INPUT');
+      expect(element[0].placeholder).toBe('00/00/0000');
+      expect(element[0].maxLength).toBe(10);
+    });
+
+    it('Should be able to create directive by attribute', function() {
+      var element = angular.element('<input n4-date-input="" data-ng-model="value">');
+      $compile(element)($scope);
+      $scope.$apply();
+
+      expect(angular.isDefined(element)).toBeTruthy();
+      expect(element[0].tagName).toBe('INPUT');
+      expect(element[0].placeholder).toBe('00/00/0000');
+      expect(element[0].maxLength).toBe(10);
+    });
+
+    it('Should be able to create directive defining a custom placeholder', function() {
+      var element = angular.element('<input n4-date-input="" data-ng-model="value" placeholder="dia/mês/ano">');
+      $compile(element)($scope);
+      $scope.$apply();
+
+      expect(angular.isDefined(element)).toBeTruthy();
+      expect(element[0].tagName).toBe('INPUT');
+      expect(element[0].placeholder).toBe('dia/mês/ano');
+      expect(element[0].maxLength).toBe(10);
+    });
+  });
+
+  describe('Functionality', function() {
+    var element;
+
+    beforeEach(inject(function() {
+      element = $compile('<input class="n4-date-input" data-ng-model="value">')($scope);
+      $scope.$apply();
+    }));
+
+    it('Should render correctly when value is a date', function() {
+      $scope.value = new Date('01/14/2015');
+      $scope.$apply();
+      expect(element.val()).toBe('14/01/2015');
+    });
+
+    it('Should render correctly when value is a valid full date string', function() {
+      $scope.value = new Date('01/14/2015').toString();
+      $scope.$apply();
+      expect(element.val()).toBe('14/01/2015');
+    });
+
+    it('Should render correctly when value is a valid date with 2 digit year', function() {
+      $scope.value = '14/01/15';
+      $scope.$apply();
+      expect(element.val()).toBe('14/01/2015');
+    });
+
+    it('Should render correctly when value is a valid string', function() {
+      $scope.value = '14/01/2015';
+      $scope.$apply();
+      expect(element.val()).toBe('14/01/2015');
+    });
+
+    it('Should render correctly when value is a valid string without backslash', function() {
+      $scope.value = '14012015';
+      $scope.$apply();
+      expect(element.val()).toBe('14/01/2015');
+    });
+
+    it('Should render an empty value to the input when the date is invalid', function() {
+      $scope.value = new Date('14/01/2015');
+      $scope.$apply();
+      expect(element.val()).toBe('');
+    });
+
+    it('Should set a Date on the scope', function() {
+      element.val('14012015');
+      element.trigger('change');
+      $scope.$apply();
+      $scope.$digest();
+      expect($scope.value).toEqual(new Date('01/14/2015'));
+    });
+
+    it('Should set null on the scope when an invalid date is set', function() {
+      element.val('01142015');
+      element.trigger('change');
+      $scope.$apply();
+      $scope.$digest();
+      expect($scope.value).toEqual(null);
+    });
+
+    it('Should set null on the scope when an empty value is set', function() {
+      element.val('01012015');
+      element.trigger('change');
+      $scope.$apply();
+      $scope.$digest();
+      element.val('');
+      element.trigger('change');
+      $scope.$apply();
+      $scope.$digest();
+      expect($scope.value).toEqual(null);
+    });
+
+    it('Should set only when value is complete', function() {
+      $scope.value = null;
+      $scope.$apply();
+      element.val('01012');
+      element.trigger('change');
+      $scope.$apply();
+      expect(element.val()).toEqual('01012');
+      expect($scope.value).toEqual(null);
+    });
+  });
+});
