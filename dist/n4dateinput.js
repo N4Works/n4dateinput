572121fedd2c4565e45d99e1b9ec6f1d45a7745f
Tiago Resende <tiagor87@gmail.com>
2015-10-12 17:03:31 -0300

    Ajuste para minificação.

--- dist/n4dateinput.js
+++ dist/n4dateinput.js
@@ -1,47 +1,47 @@
-"use strict";
-
 ;
 (function(ng) {
+  "use strict";
+
   ng
-    .module('n4DateInput', [])
-    .directive('n4DateInput', [
-      '$filter',
+    .module("n4DateInput", [])
+    .directive("n4DateInput", [
+      "$filter",
       function($filter) {
         return {
-          require: 'ngModel',
-          restrict: 'EAC',
+          require: "ngModel",
+          restrict: "EAC",
           replace: true,
-          template: '<input type="text">',
+          template: "<input type=\"text\">",
           link: function(scope, element, attrs, controller) {
             var formatValue = function(value) {
                 if (!value) {
-                  return '';
+                  return "";
                 }
 
                 var numbers, regexp, date,
                   returnValue = function(dateValue) {
-                    return isNaN(dateValue) ? '' : $filter('date')(dateValue, 'dd/MM/yyyy');
+                    return isNaN(dateValue) ? "" : $filter("date")(dateValue, "dd/MM/yyyy");
                   };
 
                 if (value instanceof Date) {
                   return returnValue(value);
                 }
 
-                numbers = value.replace(/\D/gi, '');
+                numbers = value.replace(/\D/gi, "");
 
-                regexp = new RegExp('^(\\d{2})(\\d{2})(\\d{2})$', 'gi');
+                regexp = new RegExp("^(\\d{2})(\\d{2})(\\d{2})$", "gi");
                 if (regexp.test(numbers)) {
-                  date = new Date(numbers.replace(regexp, '$2/$1/' + (attrs.century || '20') + '$3'));
+                  date = new Date(numbers.replace(regexp, "$2/$1/" + (attrs.century || "20") + "$3"));
                   return returnValue(date);
                 }
 
-                regexp = new RegExp('^(\\d{2})(\\d{2})(\\d{4})$', 'gi');
+                regexp = new RegExp("^(\\d{2})(\\d{2})(\\d{4})$", "gi");
                 if (regexp.test(numbers)) {
-                  date = new Date(numbers.replace(regexp, '$2/$1/$3'));
+                  date = new Date(numbers.replace(regexp, "$2/$1/$3"));
                   return returnValue(date);
                 }
 
-                regexp = new RegExp('^\\w{3} \\w{3} \\d{1,2} \\d{4} .*$', 'gi');
+                regexp = new RegExp("^\\w{3} \\w{3} \\d{1,2} \\d{4} .*$", "gi");
                 if (regexp.test(value)) {
                   return returnValue(new Date(value));
                 }
@@ -50,29 +50,29 @@
               },
               parseValue = function(value) {
                 var formattedValue = formatValue(value),
-                  regexp = new RegExp('^(\\d{2})/(\\d{2})/(\\d{4})$', 'gi');
+                  regexp = new RegExp("^(\\d{2})/(\\d{2})/(\\d{4})$", "gi");
 
-                var date = regexp.test(formattedValue) ? new Date(formattedValue.replace(regexp, '$2/$1/$3')) : null;
+                var date = regexp.test(formattedValue) ? new Date(formattedValue.replace(regexp, "$2/$1/$3")) : null;
                 return date;
               };
 
-            if (!element.attr('placeholder')) {
-              element.attr('placeholder', '00/00/0000');
+            if (!element.attr("placeholder")) {
+              element.attr("placeholder", "00/00/0000");
             }
-            element.attr('maxlength', 10);
+            element.attr("maxlength", 10);
 
-            element.on('blur', function () {
+            element.on("blur", function() {
               element.val(formatValue(element.val()));
             });
 
             controller.$formatters.push(formatValue);
             controller.$parsers.push(parseValue);
 
-            scope.$on('$destroy', function () {
-              element.off('blur');
+            scope.$on("$destroy", function() {
+              element.off("blur");
             });
           }
         };
       }
     ]);
-}(angular))
+}(angular));
